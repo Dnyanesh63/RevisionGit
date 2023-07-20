29805100dc45763a547262ad35f922af827df7b9
Dnyaneshwar <p.dnyaneshwar20gmail.com>
2023-07-20 13:50:27 +0530

    [Dnyaneshwar]adding New project

---
+++ src/test/java/Demo/facebbokcreateacntPage.java
@@ -0,0 +1,16 @@
+package Demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class facebbokcreateacntPage 
+{
+  @Test
+  public void f() throws InterruptedException
+  {
+	  WebDriver driver=new ChromeDriver();
+	  Thread.sleep(5000);
+	  driver.get("https://www.facebook.com/"); 
+  }
+}
